eb0064447089dfa1e33c111e5565854466060d7d
Jubair Ibn Khaled <160339037+Juba1r@users.noreply.github.com>
2024-10-01 10:28:01 +0600

    UPDATE

--- Auth Site/src/redux/cartSlice.js
+++ Auth Site/src/redux/cartSlice.js
@@ -3,22 +3,20 @@ import { createSlice } from "@reduxjs/toolkit";
 const cartSlice = createSlice({
   name: "cart",
   initialState: {
-    cartCount: 0, // Total count of items in the cart
-    items: [], // Array to hold cart items
+    cartCount: 0,
+    items: [],
   },
   reducers: {
     addToCart: (state, action) => {
       const product = action.payload;
-      state.cartCount += 1; // Increment the total count
+      state.cartCount += 1;
       const existingProduct = state.items.find(
         (item) => item.id === product.id
       );
 
       if (existingProduct) {
-        // If the product is already in the cart, increment its quantity
         existingProduct.quantity += 1;
       } else {
-        // Add the new product to the cart with a quantity of 1
         state.items.push({ ...product, quantity: 1 });
       }
     },
@@ -27,12 +25,10 @@ const cartSlice = createSlice({
       const existingProduct = state.items.find((item) => item.id === productId);
 
       if (existingProduct) {
-        // Decrement the quantity by 1
         existingProduct.quantity -= 1;
-        // Decrement the total cart count
+
         state.cartCount -= 1;
 
-        // If quantity is 0, remove the product from the cart
         if (existingProduct.quantity === 0) {
           state.items = state.items.filter((item) => item.id !== productId);
         }
@@ -41,6 +37,5 @@ const cartSlice = createSlice({
   },
 });
 
-// Export actions for use in components
 export const { addToCart, removeOneFromCart } = cartSlice.actions;
 export default cartSlice.reducer;
